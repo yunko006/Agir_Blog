71af2d82e61ff59176afc77dd355cb9ff1ecc109
yunko006 <peyrachethomas@gmail.com>
2021-11-23 18:39:24 +0100

    Update front end

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import BlogPost
+from .models import BlogPost, Category
 
 def index(request):
 
-    blog_posts = BlogPost.objects.order_by('created_on')
-    context = {'blog_posts': blog_posts}
+    blog_posts = BlogPost.objects.order_by('-created_on')
+    categories = Category.objects.all()
+    context = {'blog_posts': blog_posts, 'categories':categories}
 
     return render(request, 'blog/index.html', context)
 
