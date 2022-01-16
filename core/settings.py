c5dca93c170470be28a7569c2000466769199239
yunko006 <peyrachethomas@gmail.com>
2022-01-16 18:59:04 +0100

    requirements for heroku

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,9 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+import django_heroku
+
+django_heroku.settings(locals())
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
